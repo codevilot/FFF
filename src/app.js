d81c147181c7d4493e192544e59abb47bb06513f
namhun <namhundred@naver.com>
2022-04-29 11:57:23 +0900

    css fix
    css fix
    
    copy error fix
    
    copy error fix
    
    copy revise
    
    copy revise
    
    color pale rvise
    
    color pale rvise

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
 fetch("./src/color.json")
-    .then(function(res){
+    .then(function (res) {
         return res.json();
     })
-    .then(function(data){
+    .then(function (data) {
         const color = data.color;
         const $section = document.querySelector('#color');
         const list = []
-        for(const c in color){
+        for (const c in color) {
             const h3 = document.createElement('h3');
             const ul = document.createElement('ul');
             let number = 100;
             let combinedText = "";
             $section.appendChild(h3);
             $section.appendChild(ul);
-            
+
             ul.classList.add('colorul')
-            color[c].forEach((v)=>{
+            color[c].forEach((v) => {
                 const li = document.createElement('li');
                 const colordiv = document.createElement('div');
-                const textdiv =document.createElement('div');
-                const colortext = `var(--${c}-color-${number}) : ${v};
+                const textdiv = document.createElement('div');
+                const colortext = `--${c}-color-${number} : #${v};
                 `
 
                 combinedText = combinedText + colortext;
-                number = number+100;
+                number = number + 100;
                 ul.appendChild(li);
                 li.appendChild(colordiv);
                 li.appendChild(textdiv);
@@ -32,10 +32,10 @@ fetch("./src/color.json")
                 colordiv.style.backgroundColor = `#${v}`;
 
                 textdiv.classList.add('colorcode')
-                textdiv.innerHTML =`#${v}`
+                textdiv.innerHTML = `#${v}`
             })
             h3.innerHTML = `${c} list <span> Copy </span> <textarea class="verysmall" id="${c}">${combinedText}</textarea>`
-            h3.addEventListener("click", e=>{
+            h3.addEventListener("click", e => {
                 const value = document.querySelector(`#${c}`)
                 value.select();
                 document.execCommand("copy");
