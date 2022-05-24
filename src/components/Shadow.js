53e6171feb77ad4d8a09a4319d953bbc4e7ed1fc
namhun <namhundred@naver.com>
2022-05-24 17:17:28 +0900

    feat:add shadow load in shadow.js

--- src/components/Shadow.js
+++ src/components/Shadow.js
@@ -7,9 +7,70 @@ export default class Git extends Component {
   template() {
     // const { items } = this.$state;
     return `
-    shadow
+    <h2>Shadow</h2>
+    <ul class="shadow__wrap">
+    </ul>
     `;
   }
 
-  setEvent() {}
+  setEvent() {
+    fetch("./src/shadow.json")
+      .then(function (res) {
+        console.log(res)
+        return res.json();
+      }).then(function (data) {
+        const shadow = data.shadow;
+        const $wrap = document.querySelector(".shadow__wrap")
+        const list = [];
+        console.log(data)
+        for (const s in shadow) {
+          const li = document.createElement("li");
+          $wrap.appendChild(li);
+          li.innerHTML = `${s}`
+          li.classList.add("shadow__div")
+          li.style = shadow[s]
+        }
+      })
+    //   .then(function (data) {
+    //     const color = data.color;
+    //     const $section = document.querySelector("#color");
+    //     const list = [];
+    //     for (const c in color) {
+    //       const h3 = document.createElement("h3");
+    //       const ul = document.createElement("ul");
+    //       let number = 100;
+    //       let combinedText = "";
+    //       $section.appendChild(h3);
+    //       $section.appendChild(ul);
+
+    //       ul.classList.add("colorul");
+    //       color[c].forEach((v) => {
+    //         const li = document.createElement("li");
+    //         const colordiv = document.createElement("div");
+    //         const textdiv = document.createElement("div");
+    //         const colortext = `var(--${c}-color-${number}) : ${v};
+    //                 `;
+    //         combinedText = combinedText + colortext;
+    //         number = number + 100;
+    //         ul.appendChild(li);
+    //         li.appendChild(colordiv);
+    //         li.appendChild(textdiv);
+    //         colordiv.classList.add("colorpreview");
+    //         colordiv.style.backgroundColor = `#${v}`;
+    //         textdiv.classList.add("colorcode");
+    //         textdiv.innerHTML = `#${v}`;
+    //       });
+    //       h3.innerHTML = `${c} list <span> Copy </span> <textarea class="verysmall" id="${c}">${combinedText}</textarea>`;
+    //       h3.addEventListener("click", (e) => {
+    //         const value = document.querySelector(`#${c}`);
+    //         value.select();
+    //         document.execCommand("copy");
+    //         alert("복사되었습니다");
+    //         console.log(value);
+    //       });
+    //     }
+    //   });
+  }
+  mounted() {
+  }
 }
--- src/core/Nav.js
+++ src/core/Nav.js
@@ -9,13 +9,9 @@ export default class Nav extends Component {
   template() {
     return `
     <h2 class="pageList">Page List</h2>
-    <a class="color">Color Palette</a>
     <a class="shadow">Shadow Palette</a>
     <a class="git">Git Commit Message Convention</a>
-    <a class="style">Style default</a>
-    <a class="favorite">Recommend Site FF</a>
-    <a class="motive">-Design Motivate</a>
-    <a class="svg">-SVG Site</a>
+
     `;
   }
 }
