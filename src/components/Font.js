403537f6f06a44a61932c89e2c614d39ce6f8afb
namhun <namhundred@naver.com>
2022-05-31 21:50:43 +0900

    fix:move font.json to src/font.json

--- src/components/Font.js
+++ src/components/Font.js
@@ -7,12 +7,36 @@ export default class Font extends Component {
     template() {
         // const { items } = this.$state;
         return `
-        Open font
+        <h2>Font</h2>
+        <ul class="font__wrap">
+        </ul>
      
     `;
     }
 
     setEvent() {
+        fetch("./src/font.json")
+            .then(function (res) {
+                console.log(res)
+                return res.json();
+            }).then(function (data) {
+                const font = data.font;
+                const $wrap = document.querySelector(".font__wrap")
+                const list = [];
+                console.log(data)
+                for (const s in font) {
+                    const li = document.createElement("li");
+                    $wrap.appendChild(li);
+                    li.innerHTML = `${s}`
+                    li.classList.add("font__div")
+                    li.style = font[s]
+                }
+            })
 
+        const fontWrap = document.querySelector(".font__wrap")
+        fontWrap.addEventListener("click", (e) => {
+            const fontValue = window.getComputedStyle(e.target).getPropertyValue("box-font")
+            clickToCopy(`box-shadow:${shadowValue}`)
+        });
     }
 }
