82179cad06870496f4816bbafbca125c185c026b
namhun <namhundred@naver.com>
2022-06-01 14:26:28 +0900

    feat:add font title in font.js

--- src/components/Font.js
+++ src/components/Font.js
@@ -1,5 +1,5 @@
 import Component from "../core/Component.js";
-
+import { createSelector } from "../core/Function.js";
 export default class Font extends Component {
     setup() {
         // this.$state = { items: ["item1", "item2"] };
@@ -17,20 +17,25 @@ export default class Font extends Component {
     setEvent() {
         fetch("./src/font.json")
             .then(function (res) {
-                console.log(res)
                 return res.json();
             }).then(function (data) {
+
+                const head = document.querySelector("head")
+                const style = createSelector("style", head)
                 const font = data.font;
                 const $wrap = document.querySelector(".font__wrap")
-                const list = [];
-                console.log(data)
+
+                let fontList = ""
                 for (const s in font) {
-                    const li = document.createElement("li");
-                    $wrap.appendChild(li);
-                    li.innerHTML = `${s}`
-                    li.classList.add("font__div")
-                    li.style = font[s]
+                    const li = createSelector("li", $wrap, "font__div")
+                    const h3 = createSelector("h3", li, "font__sample")
+                    const textarea = createSelector("textarea", $wrap, "font__test")
+                    h3.style.fontFamily = font[s]
+                    h3.innerHTML = `${s}`
+                    fontList += font[s]
+                    console.log(font[s])
                 }
+                style.innerHTML = fontList
             })
 
         const fontWrap = document.querySelector(".font__wrap")
--- src/core/Function.js
+++ src/core/Function.js
@@ -1,21 +1,22 @@
 export function createSelector(selector, where, className) {
   const $selector = document.createElement(selector);
-  if(where!=="body"){
-    where.appendChild($selector) }
-    else {document.body.appendChild($selector);}
+  if (where !== "body") {
+    where.appendChild($selector)
+  }
+  else { document.body.appendChild($selector); }
   $selector.classList.add(className);
   return $selector;
 }
-export function clickToCopy (value) {
-  const tempArea = createSelector("textarea","body", "verysmall")
+export function clickToCopy(value) {
+  const tempArea = createSelector("textarea", "body", "verysmall")
   tempArea.value = value
   tempArea.select();
   document.execCommand('copy');
   document.body.removeChild(tempArea);
   alertCopy()
 }
-function alertCopy(){
-  const alert = createSelector("div","body", "alertCopy")
+function alertCopy() {
+  const alert = createSelector("div", "body", "alertCopy")
   alert.innerHTML = `Copy it!`
-  setTimeout(() => {document.body.removeChild(alert)}, 500);
+  setTimeout(() => { document.body.removeChild(alert) }, 500);
 }
